ba9a04379ea730d9cfd2484f98e4b34273fd7c72
Melanie Bareño <melanie.bareo@gmail.com>
2022-11-10 22:43:51 -0300

    carrito finalizado

--- Desktop/Mel/Javascript/EntregaFinal/main.js
+++ Desktop/Mel/Javascript/EntregaFinal/main.js
@@ -1,174 +1,64 @@
-//ACCEDER AL DOM
-// 1 - id - trae un objeto
-let titulo = document.getElementById("titulo");
-console.log(titulo);
-
-// 2 - clase - va a traer un array de objetos
-let parrafos = document.getElementsByClassName("parrafo");
-console.log(parrafos);
-
-// 3 - por etiqueta - trae un array
-let subtitulo = document.getElementsByTagName("h2");
-console.log(subtitulo);
-
-for(const parrafo of parrafos) {
-    console.log(parrafo.innerHTML)
-}
-
-// 4 - por selector
-console.log(document.querySelector("#titulo"))
-console.log(document.querySelector(".parrafo"))
-
-
-// function nombreUtil( parametros-opcional )
-
-// function mostrarNombre(){
-//     let nombre = prompt("Cual es tu nombre?");
-//     alert(`Tu nombre es ${nombre}`);
-// }
-
-// mostrarNombre();
-
-function calcularDescuento(precio, porcentaje) {
-let valorDescuento = precio * porcentaje /100;
-let precioFinal = precio - valorDescuento;
-
-return precioFinal;
-}
-
-// console.log(precioFinal); 
-
-
-//---------------- Carrito de compras input ---------------------//
-
-
-const btnCarrito = document.querySelector("#cart");
-const ventanaCarrito = document.querySelector(".cart-modal-overlay");
-const cerrarCarrito = document.querySelector("#close-btn");
-const botonesComprar = document.querySelectorAll(".add-to-cart");
-const contenedorCarrito = document.querySelector(".product-rows");
-const totalCarrito = document.querySelector('.total-price');
-const totalProducto = document.querySelector(".cart-quantity");
-let carrito = [];
-
+// Array de productos
 
 //---------------- Carga de productos usuarios internos ---------------------//
 
 
 const listaProductos = [
-    { id:1 ,nombre: "Pack  x 5- clases de yogas", categoria:"Yoga", precio: 1500}, 
-    { id:2 ,nombre: "Clase de yoga individual", categoria:"Yoga", precio: 300},
-    { id:3 ,nombre: "7 Meditaciones dinamicas con Gus",categoria:"Meditacion", precio: 4000},
-    { id:4 ,nombre: "Reescribir la mente", categoria:"Biocodificacion", precio: 2000},
-    { id:5 ,nombre: "Alineacion de Chackras - 12 dias", categoria:"Meditacion", precio: 3500},
+  { id:1 ,nombre: "Pack  x 5- clases de yogas", categoria:"Yoga", precio: 1500, imagen: './assets/img/purchase_order/yoga-group-classes.jpg'}, 
+  { id:2 ,nombre: "Clase de yoga individual", categoria:"Yoga", precio: 300, imagen: './assets/img/purchase_order/individual_yoga.jpg'},
+  { id:3 ,nombre: "7 Meditaciones dinamicas con Gus",categoria:"Meditacion", precio: 4000, imagen: './assets/img/purchase_order/meditation.jpg'},
+  { id:4 ,nombre: "Reescribir la mente", categoria:"Biocodificacion", precio: 2000, imagen: './assets/img/purchase_order/biodescodificacion.jpg'},
+  { id:5 ,nombre: "Alineacion de Chackras - 12 dias", categoria:"Meditacion", precio: 3500, imagen: './assets/img/purchase_order/chakra_alineacion.jpg'},
 ];
 
-class Producto {
-    constructor(nombre, categoria, precio) {
-        
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = precio
-    }
-}
 const guardarProducto = () => {
-    let nombre = document.getElementById("nombre").value;
-    let categoria = document.getElementById("categoria").value;
-    let precio = parseFloat(document.getElementById("precio").value);
-
-    let nuevoProd = new Producto(id,nombre, categoria, precio);
-    listaProductos.push(nuevoProd);
+  let nombre = document.getElementById("nombre").value;
+  let categoria = document.getElementById("categoria").value;
+  let precio = parseFloat(document.getElementById("precio").value);
+  let imagen = document.getElementById("imagen");
+  let nuevoProd = new Producto(id,nombre, categoria, precio, imagen);
+  listaProductos.push(nuevoProd);
 }
 
-console.log(listaProductos);
-
-
-//---------------- Create card ---------------------//
-
-/* for (let i of products.data) {
-  //Create Card
-  let card = document.createElement("div");
-  card.appendChild(container);
-  document.getElementById("products").appendChild(card);
-} */
-
-  //FOREACH - 
-listaProductos.forEach((producto)=> {
-    console.log(`este producto es ${producto.nombre} y su precio es $${producto.precio}`)
-});
-
-listaProductos.forEach((producto) => {
-let contenedorProd = document.createElement("div");
- contenedorProd.className = 'producto';
-contenedorProd.innerHTML = `
-    <h3 id='${producto.id}'>${producto.nombre}</h3>
-    <p>${producto.categoria}</p>
-    <span class="product-price">$${producto.precio}</span>
-    <button class="add-to-cart">Comprar</button>
- `;
-document.querySelector("#productos").append(contenedorProd);
-})
-
-
-
-
-
-//---------------- Busqueda de productos en la web ---------------------//
-
-//SOME 
-let buscarPorPrecio = listaProductos.some(producto => producto.precio > 3000 < 1000);
-
-console.log(buscarPorPrecio)
-
-//Parametro obtenido de boton - igual a categoria 
-function filterProduct(value) {
-    //Button class code
-    let buttons = document.querySelectorAll(".button-value");
-    buttons.forEach((button) => {
-      //check if value equals innerText
-      if (value.toUpperCase() == button.innerText.toUpperCase()) {
-        button.classList.add("active");
-      } else {
-        button.classList.remove("active");
-      }
-    });
-}
-
-//Search button click
-document.getElementById("search").addEventListener("click", () => {
-    //inicializados
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".producto.nombre");
- 
-  
-    //loop through all elements
-    elements.forEach((element, index) => {
-      //check if text includes the search value
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        //mostrar elemento
-        listaProductos[index].classList.remove("hide");
-      } else {
-        //esconder otros
-        listaProductos[index].classList.add("hide");
-      }
-    });
-  });
+  // Se captura el template de los productos
+  const templateProd = document.getElementById('template-prod').content
+  const contenedorProd = document.querySelector('.contenedor-productos')
+  const fragment = document.createDocumentFragment()
   
   
-// -----------------Carrito de compras funciones---------------------//
-//MAP - crea otro array con alguna transformacion
-/* let listaNueva = listaProductos.map(producto => producto.precio = producto.precio * 1.07);
+  // TODO LO RELACIONADO A AGREGAR LOS PRODUCTOS AL DOM
+  Object.values(listaProductos).forEach( producto => {
+    templateProd.querySelector('.div-info .nombre-prod').textContent = producto.nombre
+    templateProd.querySelector('.div-precio-boton .precio').textContent = producto.precio
+    templateProd.querySelector('.div-info .descripcion-prod').textContent = producto.categoria
+    templateProd.querySelector('.contenedor-img img').setAttribute('alt', producto.nombre)
+    templateProd.querySelector('.contenedor-img img').setAttribute('src', producto.imagen)
+    const clone = templateProd.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  contenedorProd.appendChild(fragment)
+  
+  // TODO LO RELACIONADO AL CARRITO DE COMPRA
 
-console.log(listaNueva)
+  //---------------- Carrito de compras input ---------------------//
 
-//REDUCE - resumir el array a un valor - acepta 2 parametros
+  
+  let carrito = {}
 
-const totalCarrito = listaProductos.reduce((acumulador, producto)=> {
-   return acumulador + producto.precio;
-}, 0);
-console.log(`El total es  $${totalCarrito}`); */
+  const templateTabla = document.getElementById('agregar-producto-al-carro').content
+  const tbodyCarrito = document.getElementById('carrito-body')
+  const fragmentTabla = document.createDocumentFragment()
+  const templateFoot = document.getElementById('tfooter').content
+  const tfootCarrito = document.getElementById('footer')
+  const btnCarrito = document.querySelector("#cart");
+  const ventanaCarrito = document.querySelector(".cart-modal-overlay");
+  const cerrarCarrito = document.querySelector("#close-btn");
 
-// abrir el carrito 
+  const totalCarrito = document.querySelector('.total-price');
+
+  
+  
+  // abrir el carrito 
 btnCarrito.addEventListener("click", ()=> {
   ventanaCarrito.classList.add("open");
 })
@@ -178,50 +68,98 @@ cerrarCarrito.addEventListener("click", ()=> {
   ventanaCarrito.classList.remove("open");
 })
 
-console.log(botonesComprar);
-//agregar a cada boton la funcion para agregar el producto al carrito
-botonesComprar.forEach(boton => {
-  boton.addEventListener("click", agregarCarrito);
-})
-
-
-function agregarCarrito(e){
-  boton = e.target;
-  let padre = boton.parentElement;
-  let nombreProd = padre.querySelector("h3").textContent;
-  let precio = parseFloat(padre.querySelector('.product-price').textContent.replace("$", ""));
-  
-  const prodCarrito = new Producto(nombreProd, precio);
-
-  carrito.push(prodCarrito);
-  popularCarrito();
-  ActualizarCantidadCarrito();
-}
-
-function popularCarrito(){
-  contenedorCarrito.innerHTML = '';
-  carrito.forEach(producto => {
-      contenedorCarrito.innerHTML += `
-          <div class='product-row'>
-            <span>${producto.nombreProd}</span>
-              <span class='cart-price'>$${producto.precio}</span>
-              <input type='number' value='1' class="product-quantity" />
-              <button class="remove-btn">Borrar</button>
-          </div>
-      `
+ contenedorProd.addEventListener('click', e => {
+    
+    if(e.target.textContent === "Agregar") {
+      setCarrito(e.target.parentElement.parentElement)
+    }
+    e.stopPropagation();
   })
-  actualizarTotal();
+  const setCarrito = e => {
+    const pivoteCarrito = {
+      nombre: e.querySelector('.div-info .nombre-prod').textContent,
+      precio: e.querySelector('.div-precio-boton .precio').textContent,
+      cantidad: 1
+    }
+    if(carrito.hasOwnProperty(pivoteCarrito.nombre)){
+      carrito[pivoteCarrito.nombre].cantidad += 1
+    } else {
+      carrito[pivoteCarrito.nombre] = {...pivoteCarrito}
+    }
+    pintarTabla(carrito)
+  }
   
-}
-
-function actualizarTotal() {
-  let total = carrito.reduce((acc, producto)=>{ 
-      return acc + producto.precio 
-  },0)
-  // console.log(total)
-  totalCarrito.innerHTML = `$${total}`
+  const pintarTabla = objetoCarrito => {
+    Object.values(objetoCarrito).forEach( objeto => {
+      const cloneTabla = templateTabla.cloneNode(true)
+      cloneTabla.getElementById('producto').textContent = objeto.nombre
+      cloneTabla.getElementById('cant').textContent = objeto.cantidad
+      cloneTabla.getElementById('precio-uni').textContent = objeto.precio
+      let precioTotal = parseFloat(objeto.precio) * objeto.cantidad
+      cloneTabla.getElementById('precio-total-prod').textContent = precioTotal.toFixed(2)
+      fragmentTabla.appendChild(cloneTabla)
+    })
+    tbodyCarrito.innerHTML = ''
+    tbodyCarrito.appendChild(fragmentTabla)
+    pintarFooter()
+  }
+  const pintarFooter = () => {
+    tfootCarrito.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+      tfootCarrito.innerHTML = '<tr><td colspan = 4>¡No hay ningun elemento en el carrito!</td></tr>'
+    } else {
+      const total = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + (cantidad * precio),0)
+      templateFoot.getElementById('total-a-pagar').textContent = total.toFixed(2)
+      const cloneFoot = templateFoot.cloneNode(true)
+      fragment.appendChild(cloneFoot)
+      tfootCarrito.appendChild(fragment)
+      //Boton Vaciar carrito
+      const botonVaciar = document.getElementById('vaciar-tabla')
+  botonVaciar.addEventListener('click', () => {
+        carrito = {}
+        pintarTabla(carrito)
+        pintarFooter()
+      })
+      
+      //Botones aumentar y disminuir cantidades
+      
+    }
+  }
+  tbodyCarrito.addEventListener('click', e => {
+    
+    if(e.target.classList.contains('button')) {
+      aumentarDisminuir(e.target)
+    }
+  })
+  const aumentarDisminuir = boton => {
+    if(boton.textContent === '+'){
+      const indicador = boton.parentElement.parentElement.firstElementChild.textContent
+      Object.values(carrito).forEach( elemento => {
+        if(elemento.nombre === indicador) {
+        carrito[elemento.nombre].cantidad++  
+        }
+      })
+    }
+    if(boton.textContent === '-') {
+      const indicador = boton.parentElement.parentElement.firstElementChild.textContent
+      Object.values(carrito).forEach( elemento => {
+        if(elemento.nombre === indicador) {
+        carrito[elemento.nombre].cantidad--
+          if(carrito[elemento.nombre].cantidad === 0) {
+            delete carrito[elemento.nombre]
+          }
+        }
+      })
+    }
+    pintarTabla(carrito)
+    pintarFooter()
+  }
+         
+      //Total cantidades general
+  function ActualizarCantidadCarrito () {
+    totalProducto.textContent = carrito.length;
 }
 
 function ActualizarCantidadCarrito () {
-  totalProducto.textContent = carrito.length;
+  totalCarrito.textContent = objetoCarrito.precioTotal;
 }
